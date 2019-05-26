cbf36b76b1bbe1c351c5419f8af2619ec7c41c8e
Vyacheslav Beltyukov <vyacheslav@beltyukov.mobi>
2019-05-26 11:54:05 +0300

    Initial Commit

---
+++ CharsetKeyboard/CharsetKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  CharsetKeyboard.h
+//  CharsetKeyboard
+//
+//  Created by Vyacheslav Beltyukov on 5/26/19.
+//  Copyright © 2019 Vyacheslav Beltyukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CharsetKeyboard.
+FOUNDATION_EXPORT double CharsetKeyboardVersionNumber;
+
+//! Project version string for CharsetKeyboard.
+FOUNDATION_EXPORT const unsigned char CharsetKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CharsetKeyboard/PublicHeader.h>
+
+
